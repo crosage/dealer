09a2fa3bab9c4196d9adc58793d95fdad5daa2da
crosage <90540469+crosage@users.noreply.github.com>
2024-11-13 12:59:07 +0800

    :bug: fix: 实际上是不需要@MapperScan("com.powerdealer.dealer.module.user.mapper")的，确定为mybatis版本问题导致

--- src/main/java/com/powerdealer/dealer/DealerApplication.java
+++ src/main/java/com/powerdealer/dealer/DealerApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.powerdealer.dealer.module.user.mapper")
 public class DealerApplication {
 
     public static void main(String[] args) {
